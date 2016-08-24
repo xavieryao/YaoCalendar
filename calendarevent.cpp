e86042717404e7824f570607300ff9d951b80f8c
Xavier Yao <xavieryao@live.cn>
2016-08-24 11:26:39 +0800

    define repeat mode

--- calendarevent.cpp
+++ calendarevent.cpp
@@ -4,3 +4,84 @@ CalendarEvent::CalendarEvent(QObject *parent) : QObject(parent)
 {
 
 }
+
+// Auto-generated getter and setter member functions,
+QString CalendarEvent::eventName() const
+{
+    return mEventName;
+}
+
+void CalendarEvent::setEventName(const QString &eventName)
+{
+    mEventName = eventName;
+}
+
+QString CalendarEvent::location() const
+{
+    return mLocation;
+}
+
+void CalendarEvent::setLocation(const QString &location)
+{
+    mLocation = location;
+}
+
+QDateTime CalendarEvent::startTime() const
+{
+    return mStartTime;
+}
+
+void CalendarEvent::setStartTime(const QDateTime &startTime)
+{
+    mStartTime = startTime;
+}
+
+QDateTime CalendarEvent::endTime() const
+{
+    return mEndTime;
+}
+
+void CalendarEvent::setEndTime(const QDateTime &endTime)
+{
+    mEndTime = endTime;
+}
+
+QColor CalendarEvent::color() const
+{
+    return mColor;
+}
+
+void CalendarEvent::setColor(const QColor &color)
+{
+    mColor = color;
+}
+
+bool CalendarEvent::isAllDayEvent() const
+{
+    return mIsAllDayEvent;
+}
+
+void CalendarEvent::setIsAllDayEvent(bool isAllDayEvent)
+{
+    mIsAllDayEvent = isAllDayEvent;
+}
+
+QString CalendarEvent::detail() const
+{
+    return mDetail;
+}
+
+void CalendarEvent::setDetail(const QString &detail)
+{
+    mDetail = detail;
+}
+
+RepeatOptions CalendarEvent::repeatOpt() const
+{
+    return mRepeatOpt;
+}
+
+void CalendarEvent::setRepeatOpt(const RepeatOptions &repeatOpt)
+{
+    mRepeatOpt = repeatOpt;
+}
--- calendarevent.h
+++ calendarevent.h
@@ -2,16 +2,54 @@
 #define CALENDAREVENT_H
 
 #include <QObject>
+#include <QDateTime>
+#include <QColor>
+#include "repeatoptions.h"
 
 class CalendarEvent : public QObject
 {
     Q_OBJECT
 public:
     explicit CalendarEvent(QObject *parent = 0);
+    QString eventName() const;
+    void setEventName(const QString &eventName);
+
+    QString location() const;
+    void setLocation(const QString &location);
+
+    QDateTime startTime() const;
+    void setStartTime(const QDateTime &startTime);
+
+    QDateTime endTime() const;
+    void setEndTime(const QDateTime &endTime);
+
+    QColor color() const;
+    void setColor(const QColor &color);
+
+    bool isAllDayEvent() const;
+    void setIsAllDayEvent(bool isAllDayEvent);
+
+    QString detail() const;
+    void setDetail(const QString &detail);
 
 signals:
 
 public slots:
+
+private:
+    QString mEventName;
+    QString mLocation;
+    QDateTime mStartTime;
+    QDateTime mEndTime;
+    QColor mColor;
+    bool mIsAllDayEvent;
+    QString mDetail;
+    RepeatMode mRepeatMode;
+};
+
+enum RepeatMode
+{
+    NONE, PER_DAY_OF_WEEK, PER_DAY, PER_MONTH, PER_YEAR
 };
 
 #endif // CALENDAREVENT_H
